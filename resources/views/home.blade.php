d9c9b61254b581bc400f6b176d8e80521479a9e2
MyUsername <alberto.baglivi@gmail.com>
2022-12-16 16:35:11 +0100

    Add the cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,25 @@
 
 
 @section('content')
-    <h1>Hello I am the Home Page</h1>
+    <h1>Lista dei film</h1>
     <section>
-        <ul>
-            @foreach ($movies as $movie)
-                <li>
-                    <img src="{{ $movie->image }}" alt="">
-                    <h3>{{ $movie->title }}</h3>
-                </li>
-            @endforeach
-        </ul>
+        <div class="container">
+            <div class="row justify-content-between">
+                @foreach ($movies as $movie)
+                    <div class="card col-4 mb-5" style="width: 18rem;">
+                        <img src="{{ $movie->image }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $movie->title }}</h5>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </section>
 @endsection
+
+
+{{-- <li>
+                    <img src="{{ $movie->image }}" alt="">
+                    <h3>{{ $movie->title }}</h3>
+                </li> --}}
