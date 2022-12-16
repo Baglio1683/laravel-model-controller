4c415df2b319731996bc36d999a8c50a740e00f7
MyUsername <alberto.baglivi@gmail.com>
2022-12-16 16:07:35 +0100

    Add the movie list without the card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.app')
+
+
 @section('content')
-    <h1>Hello I am the Home</h1>
+    <h1>Hello I am the Home Page</h1>
+    <section>
+        <ul>
+            @foreach ($movies as $movie)
+                <li>
+                    <img src="{{ $movie->image }}" alt="">
+                    <h3>{{ $movie->title }}</h3>
+                </li>
+            @endforeach
+        </ul>
+    </section>
 @endsection
